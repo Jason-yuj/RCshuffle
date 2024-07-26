1eb02ab15fc6347b938ccf56ed10d73f59d81a4d
Jason-yuj <jasonyu1417@gmail.com>
2024-07-26 15:12:14 +0800

    update

--- Data/generate.py
+++ Data/generate.py
@@ -18,6 +18,17 @@ def generate_uniform(n, B):
     file.close()
 
 
+def generate_uniform_sample(n, B):
+    file = open("uniform_sample.txt", 'w')
+    # file.write(str(n)+'\n')
+    # file.write(str(B)+'\n')
+    for i in range(B):
+        # i = np.random.randint(0, B)
+        file.write(str(i)+'\n')
+    print("finish")
+    file.close()
+
+
 class Zipfian:
     def __init__(self, alpha):
         self.alpha = alpha
@@ -65,19 +76,56 @@ def gen_gaussian(n, B):
     file.close()
 
 
+def generate_2D(n):
+    file = open("2d_uniform.txt", "w")
+    # for i in range(32):
+    #     for j in range(32):
+    for i in range(n):
+            i = np.random.randint(0, 32)
+            j = np.random.randint(0, 32)
+            file.write(str(int(i)) + " " + str(int(j)) + '\n')
+    print("finish")
+    file.close()
+
+
+def generate_cube_sample(n):
+    file = open("cube_sample.txt", "w")
+    for x in range(8):
+        for y in range(8):
+            for m in range(4):
+                for n in range(4):
+                    file.write(str(int(x)) + " " + str(int(y)) + " " + str(int(m)) + " " + str(int(n)) + '\n')
+    print("finish")
+    file.close()
+
+
+def generate_cube(n):
+    file = open("cube.txt", "w")
+    for _ in range(n):
+        x = np.random.randint(0, 8)
+        y = np.random.randint(0, 8)
+        m = np.random.randint(0, 4)
+        n = np.random.randint(0, 4)
+        file.write(str(int(x)) + " " + str(int(y)) + " " + str(int(m)) + " " + str(int(n)) + '\n')
+    print("finish")
+    file.close()
+
+
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='optimal small domain range counting for shuffle model')
-    parser.add_argument('--n', type=int, help='total number of user')
-    parser.add_argument('--B', '--b', type=int, help='domain range')
-    parser.add_argument('--dataset', type=str, default='uniform',
-                        help='input data set')
-    opt = parser.parse_args()
-    n = opt.n
-    b = opt.B
-    t = opt.dataset
-    if t == "uniform":
-        generate_uniform(n, b)
-    elif t == "gaussian":
-        gen_gaussian(n, b)
-    else:
-        gen_zipf(n, b)
+    # parser = argparse.ArgumentParser(description='optimal small domain range counting for shuffle model')
+    # parser.add_argument('--n', type=int, help='total number of user')
+    # parser.add_argument('--B', '--b', type=int, help='domain range')
+    # parser.add_argument('--dataset', type=str, default='uniform',
+    #                     help='input data set')
+    # opt = parser.parse_args()
+    # n = opt.n
+    # b = opt.B
+    # t = opt.dataset
+    # if t == "uniform":
+    #     generate_uniform(n, b)
+    # elif t == "gaussian":
+    #     gen_gaussian(n, b)
+    # else:
+    #     gen_zipf(n, b)
+    # generate_uniform(10000000, pow(2, 30))
+    generate_cube(10000000)
